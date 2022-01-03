6ff54398450c0f500930e35a57b88e20884e1d7f
Georgie Sidziuk <georgiesidziuk@gmail.com>
2022-01-03 17:32:20 +0300

    final

---
+++ src/core/component.js
@@ -0,0 +1,17 @@
+// получение каждого корневого элемента блока
+export class Component {
+  constructor(id) {
+    this.$el = document.querySelector(id);
+    this.init();
+  }
+
+  init() {}
+
+  hide() {
+    this.$el.classList.add("hide");
+  }
+
+  show() {
+    this.$el.classList.remove("hide");
+  }
+}
